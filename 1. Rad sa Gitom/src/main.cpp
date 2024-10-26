320ed76bf9e826862a9bddc7742ac38cc1f75bfc
Jovan Drazic <jovan.drazic@student.etf.unibl.org>
2024-10-26 15:26:37 +0200

    Костур пројекта и провјера имена и презимена

---
+++ 1. Rad sa Gitom/src/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../inc/posiljalac.h"
+
+// Главни програм
+int main(void)
+{
+    // Тест
+    /*Posiljalac p;
+    char a[10] = "Janko";
+    char b[10] = "Yankov";
+    p.provjeraImena(a);
+    p.provjeraPrezimena(b);
+    */
+    
+    // Кориснички интерфејс
+    printf("%s", "\nHello World");
+    return 0;
+}
